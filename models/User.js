fae1bfd952549fe6270dba3f140827facf710103
Gessé Carlos <gesse.dev@outlook.com>
2022-02-03 12:02:24 -0300

    create user model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,14 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
-    'User',
+    'Users',
     {
       displayName: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
       image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
     },
   );
 
