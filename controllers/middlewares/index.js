d9c8e02bbc9a753a8eb2857d07c01573c73f645e
Gessé Carlos <gesse.dev@outlook.com>
2022-02-03 15:14:19 -0300

    update index js

--- controllers/middlewares/index.js
+++ controllers/middlewares/index.js
@@ -1,7 +1,13 @@
 const domainError = require('./domain-error');
 const serverError = require('./server-error');
+const jwtMiddleware = require('./jwtMiddleware');
+const loginMiddlewares = require('./loginMiddlewares');
+const userMiddlewares = require('./userMiddlewares');
 
 module.exports = {
   domainError,
   serverError,
+  jwtMiddleware,
+  loginMiddlewares,
+  userMiddlewares,
 };
